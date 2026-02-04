0eb1f52da79dd783a903c7d9ee6bec3bbcc16b0c
Helioki <eliiorestea@gmail.com>
2026-02-05 07:42:25 +0800

    slide 2

---
+++ c#-aoop/laboratory/escapesequence.cs
@@ -0,0 +1,7 @@
+Console.WriteLine("This will give a new line after.\n");
+Console.WriteLine("Do you remember, the 21st night of \tSeptember");
+Console.WriteLine("Love was changin' the minds of \"pretender");
+Console.WriteLine("While chasin\' the clouds away");
+Console.WriteLine("This is a backslash: \\");
+Console.WriteLine("Anya\bForger");
+Console.WriteLine("Carriage return:\rOverwritten text");
