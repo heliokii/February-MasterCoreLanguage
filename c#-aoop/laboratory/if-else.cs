6916fe73974d07b9dd1adb249bf1c41974350551
Helioki <eliiorestea@gmail.com>
2026-02-05 07:54:50 +0800

    exception handling

---
+++ c#-aoop/laboratory/if-else.cs
@@ -0,0 +1,7 @@
+Console.WriteLine("Enter a floating-point number: ");
+string input = Console.ReadLine();
+    if (double.TryParse(input, out double floatValue)){
+        Console.WriteLine("You entered: " + floatValue);
+    } else {
+        Console.WriteLine("Invalid input. Please enter a valid floating-point number.");
+    }
