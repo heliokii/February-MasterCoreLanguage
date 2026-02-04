ffcdbced23f6e1ef208e0fa6d58dfa200be775e0
Helioki <eliiorestea@gmail.com>
2026-02-05 07:49:22 +0800

    formatting

---
+++ c#-aoop/laboratory/formatting.cs
@@ -0,0 +1,9 @@
+int epNum = 16;
+double tvRating = 4.3456;
+char grade = 'A';
+string seriesTitle = "Love Scout";
+
+Console.WriteLine("Episodes: {0:D5}", epNum);
+Console.WriteLine("TV Rating: {0:F2}", tvRating);
+Console.WriteLine("TV Grade: {0}", grade);
+Console.WriteLine("Series Title: \"{0}\"");
