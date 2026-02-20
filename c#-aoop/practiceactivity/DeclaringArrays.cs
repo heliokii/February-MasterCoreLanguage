2f8127e52e2782b1fd3306b6c56640142c2ebcf0
Helioki <eliiorestea@gmail.com>
2026-02-20 09:52:03 +0800

    CoddyTech

---
+++ c#-aoop/practiceactivity/DeclaringArrays.cs
@@ -0,0 +1,12 @@
+public class Program {
+    public static void Main(string[] args) {
+        // Create the shoppingList array here
+        string[] shoppingList = {"bread", "eggs", "milk", "butter"};
+        
+        // Don't change the code below
+        System.Console.WriteLine("Shopping List:");
+        for (int i = 0; i < shoppingList.Length; i++) {
+            System.Console.WriteLine(shoppingList[i]);
+        }
+    }
+}
