2536d6e3fcf9eb4cd3735caaee387a49242150ef
Helioki <eliiorestea@gmail.com>
2026-02-20 09:37:55 +0800

    CoddyTech

---
+++ c#-aoop/practiceactivity/OptionalParameters.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Program {
+    // Create the DisplayMessage method here
+        public static void DisplayMessage(string message, int repeatCount = 1){
+            for (int i = 0; i < repeatCount; i++){
+                Console.WriteLine(message);
+            }
+        }
+
+    public static void Main(string[] args) {
+        DisplayMessage("Hello!");
+        DisplayMessage("Coddy", 3);
+    }
+}
